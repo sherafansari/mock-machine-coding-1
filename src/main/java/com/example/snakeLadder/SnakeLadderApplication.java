97ad75915fe86fb5f2437d717ea84269a0f52192
sherafansari <sheraf.ansari@gmail.com>
2020-06-16 15:18:26 +0530

    springboot setup

---
+++ src/main/java/com/example/snakeLadder/SnakeLadderApplication.java
@@ -0,0 +1,13 @@
+package com.example.snakeLadder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeLadderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeLadderApplication.class, args);
+	}
+
+}
